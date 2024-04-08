7986f21637c5a0cb2adf17a52ba459c96a134615
Rakeshpft <rakesh@factorfox.com>
2024-04-08 13:24:37 +0530

    8th April 3rd update

--- src/components/login-page/forgetResetPassword.tsx
+++ src/components/login-page/forgetResetPassword.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect,  useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import {
   Button,
@@ -28,59 +28,62 @@ export interface IForgotResetPassword {
   confirmPassword: string;
 }
 const ForgotResetPassword = () => {
-
   const initialForgotVerifyPass = {
     password: "",
     confirmPassword: "",
   };
 
-  const {  postForgotPassword , verifyForgotPasswordLink } = useRegContext();
+  const { postForgotPassword, verifyForgotPasswordLink } = useRegContext();
   const navigate = useNavigate();
-  // const [apiResponseMsg, setApiResponseMsg] = useState("");
-  // const [showRegistrationMessage, setShowRegistrationMessage] = useState(false);
-  // const [linkStatus, setLinkStatus] = useState(false);
+  const [apiResponseMsg, setApiResponseMsg] = useState("");
+  const [showRegistrationMessage, setShowRegistrationMessage] = useState(false);
+  const [linkStatus, setLinkStatus] = useState(false);
 
-//   const [emailStatus, setEmailStatus] = useState(null);
-//   const [emailMessage, setEmailMessage] = useState("");
+  const [emailStatus, setEmailStatus] = useState(null);
+  const [emailMessage, setEmailMessage] = useState("");
 
-  const [verifyPass, setVerifyPass] =
-    useState<IForgotResetPassword>(initialForgotVerifyPass);
+  const [verifyPass, setVerifyPass] = useState<IForgotResetPassword>(
+    initialForgotVerifyPass
+  );
 
   const params = useParams();
 
   const company_guid = params.id;
- 
+
   const handleVerifyInput =
     (prop: keyof IForgotResetPassword) =>
     (event: React.ChangeEvent<HTMLInputElement>) => {
       setVerifyPass({ ...verifyPass, [prop]: event.target.value });
     };
 
-    
-   
-      useEffect(() => {
-        verifyForgotPasswordLink(company_guid).then((data) => {
-          console.log("showEmail", data);
-          data && toastify({ message: data.message, type: (data.success ? "success" : "error") });
-          // data && !data.status && setLinkStatus(true);
-          // data && setApiResponseMsg(data.message)
+  useEffect(() => {
+    verifyForgotPasswordLink(company_guid).then((data) => {
+      console.log("showEmail", data);
+      data &&
+        toastify({
+          message: data.message,
+          type: data.success ? "success" : "error",
         });
-      }, [company_guid]);
+       data && !data.status && setLinkStatus(true);
+       data && setApiResponseMsg(data.message)
+       data && setEmailStatus(data.value);
+       data && setEmailMessage(data.message);
+    });
+  }, [company_guid]);
   const handleVerifyPassword = (event: { preventDefault: () => void }) => {
-    
     event.preventDefault();
 
-    postForgotPassword(verifyPass, company_guid).then(( data ) => {
-      data &&   toastify({ message: data.message, type: (data.success ? "success" : "error") });
+    postForgotPassword(verifyPass, company_guid).then((data) => {
+      data && setApiResponseMsg(data.message);
+      data && setShowRegistrationMessage(true);
+      setVerifyPass(initialForgotVerifyPass);
       console.log("verifyPass from forgetpage", data);
       setVerifyPass(initialForgotVerifyPass);
-     
     });
   };
-const navigateToLogin = () => {
-  navigate("/");
-}
-
+  const navigateToLogin = () => {
+    navigate("/");
+  };
 
   return (
     <>
@@ -101,99 +104,110 @@ const navigateToLogin = () => {
                     </div>
                   </Col>
                 </Row>
-                {/* {showRegistrationMessage || linkStatus ? (
+                {showRegistrationMessage || linkStatus ? (
                   <div className="text-center">
                     <h5 className="text-success text-center my-3">
                       {apiResponseMsg}
-                    </h5> */}
+                    </h5>
 
                     <Button
-                            color="primary"
-                            className="px-4 py-2 shadow save-button mx-3"
-                            onClick={navigateToLogin}
-                          >
-                            Back to Login
-                          </Button>
-                 
-)                 : (
+                      color="primary"
+                      className="px-4 py-2 shadow save-button mx-3"
+                      onClick={navigateToLogin}
+                    >
+                      Back to Login
+                    </Button>
+                  </div>
+                ) : (
                   <>
                     <Row>
                       <Col sm={10} className="mx-auto">
-                       
-                          <Form
-                            className="mt-5"
-                            onSubmit={handleVerifyPassword}
-                          >
-                            <FormGroup>
-                              <InputGroup>
-                                <InputGroupText className="d-block d-sm-inline fw-bold resetPassword">
-                                  Password
-                                </InputGroupText>
-                                <Input
-                                  type="password"
-                                  required
-                                  name="verifyPass.password"
-                                  value={verifyPass.password}
-                                  onChange={handleVerifyInput("password")}
-                                  placeholder="Enter Your Password"
-                                  autoComplete="off"
-                                  // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                                >
-                                  <AiOutlineEyeInvisible />
-                                </Input>
-
-                                <FormText color="danger"></FormText>
-                              </InputGroup>
-                            </FormGroup>
-                            <FormGroup>
-                              <InputGroup>
-                                <InputGroupText className="d-block d-sm-inline fw-bold ">
-                                  Confirm Password&nbsp;
-                                </InputGroupText>
-                                <Input
-                                  type="password"
-                                  required
-                                  name="confirm password"
-                                  value={verifyPass.confirmPassword}
-                                  onChange={handleVerifyInput(
-                                    "confirmPassword"
-                                  )}
-                                  placeholder="Enter Your Confirm Password"
-                                />
-                              </InputGroup>
-                            </FormGroup>
-
-                            <FormGroup className="text-center mt-2" >
-                              <Button
-                                color="primary"
-                                className="px-4 py-2 shadow save-button"
-                                type="submit"
-                                disabled={
-                                  !every(
-                                    verifyPass,
-                                    (data) =>
-                                      data !== "" &&
-                                      data === verifyPass.confirmPassword
-                                  )
-                                }
+                        { emailStatus ? (
+                          <>
+                          <div className="text-center my-4">
+                            <p className="fw-bold">{emailMessage}</p>
+                          </div>
+                          <FormGroup className="text-center">
+                          <Button
+                          color="primary"
+                          className="px-4 py-2 shadow save-button mx-3"
+                          onClick={navigateToLogin}
+                        >
+                          Back to Login
+                        </Button>
+                          </FormGroup>
+                        </>
+                      )
+                        : (
+                        <Form className="mt-5" onSubmit={handleVerifyPassword}>
+                          <FormGroup>
+                            <InputGroup>
+                              <InputGroupText className="d-block d-sm-inline fw-bold resetPassword">
+                                Password
+                              </InputGroupText>
+                              <Input
+                                type="password"
+                                required
+                                name="verifyPass.password"
+                                value={verifyPass.password}
+                                onChange={handleVerifyInput("password")}
+                                placeholder="Enter Your Password"
+                                autoComplete="off"
+                                // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                               >
-                                Save Password
-                              </Button>
-                            </FormGroup>
-                            <FormGroup className="text-center">
-                              <Link
-                                to={"/"}
-                                className="px-0 btn btn-link text-decoration-none fw-bold "
-                              >
-                                Back to Login
-                              </Link>
-                            </FormGroup>
-                          </Form>
-                     
+                                <AiOutlineEyeInvisible />
+                              </Input>
+
+                              <FormText color="danger"></FormText>
+                            </InputGroup>
+                          </FormGroup>
+                          <FormGroup>
+                            <InputGroup>
+                              <InputGroupText className="d-block d-sm-inline fw-bold ">
+                                Confirm Password&nbsp;
+                              </InputGroupText>
+                              <Input
+                                type="password"
+                                required
+                                name="confirm password"
+                                value={verifyPass.confirmPassword}
+                                onChange={handleVerifyInput("confirmPassword")}
+                                placeholder="Enter Your Confirm Password"
+                              />
+                            </InputGroup>
+                          </FormGroup>
+
+                          <FormGroup className="text-center mt-2">
+                            <Button
+                              color="primary"
+                              className="px-4 py-2 shadow save-button"
+                              type="submit"
+                              disabled={
+                                !every(
+                                  verifyPass,
+                                  (data) =>
+                                    data !== "" &&
+                                    data === verifyPass.confirmPassword
+                                )
+                              }
+                            >
+                              Save Password
+                            </Button>
+                          </FormGroup>
+                          <FormGroup className="text-center">
+                            <Link
+                              to={"/"}
+                              className="px-0 btn btn-link text-decoration-none fw-bold "
+                            >
+                              Back to Login
+                            </Link>
+                          </FormGroup>
+                        </Form>
+                        )}
                       </Col>
                     </Row>
                   </>
-                )
+                )}
               </Container>
             </Col>
           </Row>
